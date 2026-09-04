80cac8e162795335b78febde26f6cd8d6c24dccd
Nguyễn Nam <soniclolgaming@gmail.com>
2026-09-04 11:42:19 +0700

    Route numeric seas to icon build

--- games/7449423635.lua
+++ games/7449423635.lua
@@ -1,2 +1,2 @@
-local source = game:HttpGet("https://raw.githubusercontent.com/Ic0u/austina/a21f432/games/2753915549.lua")
+local source = game:HttpGet("https://raw.githubusercontent.com/Ic0u/austina/c244974/games/2753915549.lua")
 return assert(loadstring(source))()
